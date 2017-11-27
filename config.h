dd83e623643102aec3df9df1af6ab23c008a4ec5
Haziq Noor <haziq@protonmail.com>
2017-11-27 14:55:29 +0800

    Syncing colors

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#b2b2b2", "#080808" },
-	[SchemeSel] = { "#870000", "#080808" },
-	[SchemeOut] = { "#870000", "#080808" },
+	[SchemeNorm] = { "#b2b2b2", "#000" },
+	[SchemeSel] = { "#af0000", "#000" },
+	[SchemeOut] = { "#af0000", "#000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
